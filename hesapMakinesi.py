e4d9f17e8681ed96bf76593c1f7ad8104240d7d0
cigdem-can <16008125011@ogr.bozok.edu.tr>
2025-10-19 10:59:24 +0300

    hesapMakinesi.py oluşturuldu

---
+++ hesapMakinesi.py
@@ -0,0 +1,10 @@
+a= float(input("1.sayıyı gir:"))
+b= float(input("2.sayıyı gir:"))
+islem= input("İşlem seç (+, -, *, /):")
+
+if islem == "+":
+    print ("Sonuç:", a+b)
+elif islem== "-":   
+    print ("Sonuç:", a-b)
+    
+
